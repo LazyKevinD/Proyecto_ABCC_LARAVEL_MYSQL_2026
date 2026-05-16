061c16993d2b2c10f0f6b796bbc757fed63982fa
LazyKevinD <kevin1dany@gmail.com>
2026-05-15 21:44:44 -0600

    Docker render deploy

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\AlumnoController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\AlumnoController;
 
 Route::resource('alumnos', AlumnoController::class);
+
+Route::get('/', function () {
+    return redirect()->route('alumnos.index');
+});
